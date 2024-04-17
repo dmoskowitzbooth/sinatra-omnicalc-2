2153c9482a15e4bba255f456003add45ba8263f6
dmoskowitzbooth <165198611+dmoskowitzbooth@users.noreply.github.com>
2024-04-17 16:56:23 +0000

    3

--- app.rb
+++ app.rb
@@ -13,7 +13,7 @@ get("/wizard_add") do
   @first_num = params.fetch("first_num").to_f
   @second_num = params.fetch("second_num").to_f
 
-  @result = @first_num + @second_num
+  @sum = @first_num + @second_num
   erb(:add_result)
 end
 
